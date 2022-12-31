f9fdd1b9982ae4417b2f12a664aee83f262f0058
Wilhelm Söderkvist Vermelin <wilhelm.wermelin@icloud.com>
2022-12-31 14:21:05 +0100

    cli help text and better commands

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,28 @@
 use clap::Parser;
 use maze_gen::{generate, solve, Maze};
 
-#[derive(Parser)]
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
 struct Cli {
-    #[clap(long)]
-    width: usize,
-    #[clap(long)]
-    height: usize,
-    #[clap(long, short, action)]
-    print: bool,
-    #[clap(long, short, action)]
+    /// Width of the maze
+    #[clap(long, short, default_value_t = 16)]
+    columns: usize,
+    /// Height of the maze
+    #[clap(long, short, default_value_t = 16)]
+    rows: usize,
+    /// Don't print the maze to stdout
+    #[clap(long, action, default_value_t = false)]
+    silent: bool,
+    /// Solve the maze
+    #[clap(long, short, action, default_value_t = false)]
     solve: bool,
 }
 pub fn main() {
     let args = Cli::parse();
-    let mut maze = Maze::new(args.width, args.height);
+    let mut maze = Maze::new(args.columns, args.rows);
     generate(&mut maze);
     if args.solve {
-        solve(&mut maze, (0, 0), (args.width - 1, args.height - 1));
+        solve(&mut maze, (0, 0), (args.columns - 1, args.rows - 1));
     }
-    maze.print(Some("out.txt"), args.print).unwrap();
+    maze.print(Some("out.txt"), !args.silent).unwrap();
 }
